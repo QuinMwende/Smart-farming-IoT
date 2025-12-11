14c5fa8a284fe0a0df9c0459729c8fc726025da9
QuinMwende <quinmwende@gmail.com>
2025-12-11 16:59:17 +0300

    first commit

---
+++ src/main/java/com/example/smart_farming_iot/SmartFarmingIoTApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_farming_iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmingIoTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFarmingIoTApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smart_farming_iot/SmartFarmingIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_farming_iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmingIoTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
